a00cf0c805a8f3ae770c3a5d7375b77bab18f997
andre <andre@localhost.localdomain>
2018-03-02 13:54:44 -0300

    consulta todos os sites através de promisses, mas está com bug ao consultar a live.

--- controllers/servicosController.js
+++ controllers/servicosController.js
@@ -3,50 +3,118 @@ const Nightmare = require('nightmare')
 
 module.exports = function (app) {
 
+  let   dados = {};
+  app.get('/consulta', function (req,res) {
 
-  app.get('/netflix', function (req,res) {
 
-    var pesadelo = new Nightmare({ show: false });
-    pesadelo
-      .goto('https://help.netflix.com/pt/is-netflix-down')
-        .wait('.down-notification-content')
-      // .evaluate(() => document.querySelector('.down-notification-content h2').innerText)
-      .evaluate(function () {
-        var mensagem = document.querySelector('.down-notification-content h2').innerText;
-        return mensagem;
-      })
-      .end()
-      .then(function (data) {
-        console.log(data);
-        res.json(data);
-
-        // return;
-      })
-      .catch(error => {
-        console.error('Search failed:', error)
-      })
+    netflix(dados)
+    .then(psn(dados))
+    .then(xboxLive(dados))
+    .then(function () {
+      res.json(dados);
+    });
 
 
   });
 
-  app.get('/psn',function (req,res) {
-    var pesadelo = new Nightmare({ show: false });
-    pesadelo
-      .goto('https://status.playstation.com/pt-BR/')
-      .wait('.summaryTitleOK')
-      // .evaluate(() => document.querySelector('.summaryTitleOK').innerText)
-      .evaluate(function () {
-        var mensagem = document.querySelector('.summaryTitleOK').innerText;
-
-      })
-      .end()
-      .then(function (data) {
-        console.log(data);
-        res.json(data);
-        // return;
-      })
-      .catch(error => {
-        console.error('Search failed:', error)
-      })
+  app.get('/live', function(req,res) {
+    xboxLive(dados)
+    .then(function () {
+      res.json(dados);
+    });
   });
+
+  // Promessas de consulta do nightmare
+
+  function netflix (dados) {
+    var pesadelo = new Nightmare({ show: false });
+
+    var promessa = new Promise(
+      function (resolve,reject) {
+        pesadelo
+          .goto('https://help.netflix.com/pt/is-netflix-down')
+            .wait('.down-notification-content')
+          // .evaluate(() => document.querySelector('.down-notification-content h2').innerText)
+          .evaluate(function () {
+            var mensagem = document.querySelector('.down-notification-content h2').innerText;
+            return mensagem;
+          })
+          .end()
+          .then(function (data) {
+            console.log(data);
+            dados.netflix = data;
+            resolve(dados.netflix)
+          })
+          .catch(error => {
+            console.error('Search failed:', error)
+            reject(error);
+          })
+      }
+
+    );
+
+      return promessa;
+  }
+
+
+  function psn (dados) {
+    var pesadelo = new Nightmare({ show: false });
+
+    var promessa = new Promise(
+
+      function (resolve,reject) {
+      pesadelo
+        .goto('https://status.playstation.com/pt-BR/')
+        .wait('.summaryTitleOK')
+        // .evaluate(() => document.querySelector('.summaryTitleOK').innerText)
+        .evaluate(function () {
+          var mensagem = document.querySelector('.summaryTitleOK').innerText;
+          return mensagem;
+        })
+        .end()
+        .then(function (data) {
+          console.log(data);
+          dados.psn = data;
+          resolve(dados.psn);
+        })
+        .catch(error => {
+          console.error('Search failed:', error)
+          reject(error);
+        })
+
+      }
+    );
+    return promessa;
+  }
+
+
+  function xboxLive (dados) {
+    var pesadelo = new Nightmare({ show: false });
+
+    var promessa = new Promise(
+
+      function (resolve,reject) {
+      pesadelo
+        .goto('https://support.xbox.com/pt-BR/xbox-live-status')
+         // .wait('#ServiosprincipaisdoXboxLive h3')
+        // .evaluate(() => document.querySelector('.summaryTitleOK').innerText)
+        .evaluate(function () {
+          var mensagem = document.querySelector('#ServiosprincipaisdoXboxLive h3').text;
+          return mensagem;
+        })
+        .end()
+        .then(function (data) {
+          console.log(data);
+          dados.xboxlive = data;
+          resolve(dados.xboxlive);
+        })
+        .catch(error => {
+          console.error('Search failed:', error)
+          reject(error);
+        })
+
+      }
+    );
+    return promessa;
+  }
 }
